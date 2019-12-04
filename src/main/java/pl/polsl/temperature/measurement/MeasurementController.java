f0ac38ad12626aa393b8e8b566d39a2b6ef1176c
SzGajdzica <sz.gajdzica@labmatic.com>
2019-12-04 17:33:31 +0100

    updated post for multiple models

--- src/main/java/pl/polsl/temperature/measurement/MeasurementController.java
+++ src/main/java/pl/polsl/temperature/measurement/MeasurementController.java
@@ -5,8 +5,12 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.polsl.temperature.base.Message;
 import pl.polsl.temperature.station.Station;
 import pl.polsl.temperature.station.StationRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -16,15 +20,24 @@ public class MeasurementController {
     private final MeasurementRepository measurementRepository;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Measurement addMeasurement(@RequestBody MeasurementPost measurementPost) throws Exception {
-        Station station = stationRepository.findById(measurementPost.getStationId()).orElseThrow(Exception::new);
-        if(!station.getSecretId().equals(measurementPost.getSecretId()))
-            throw new Exception();
-        Measurement measurement = new Measurement();
-        measurement.setMeasurementTypeId(measurementPost.getMeasurementTypeId());
-        measurement.setStationId(measurementPost.getStationId());
-        measurement.setValue(measurementPost.getValue());
-        return measurementRepository.save(measurement);
+    public Message addMeasurements(@RequestBody List<MeasurementPost> measurementsPost) {
+        int successCounter = 0;
+        for(MeasurementPost measurementPost: measurementsPost){
+            Optional<Station> station = stationRepository.findById(measurementPost.getStationId());
+            if(station.isPresent() && station.get().getSecretId().equals(measurementPost.getSecretId())){
+                Measurement measurement = new Measurement();
+                measurement.setMeasurementTypeId(measurementPost.getMeasurementTypeId());
+                measurement.setStationId(measurementPost.getStationId());
+                measurement.setValue(measurementPost.getValue());
+                try {
+                    measurementRepository.save(measurement);
+                    successCounter++;
+                }catch (Exception ignore){
+
+                }
+            }
+        }
+        return new Message("Post results", successCounter + " succeeded and " + (measurementsPost.size() - successCounter) + " failed");
     }
 
 }
